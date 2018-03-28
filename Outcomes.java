b9172773d23d857c229ec05a3fdde7a3a969346e
ArtMaguire <a.elnino11@gmail.com>
2018-03-28 14:56:48 +0100

    Random Fixture automation

---
+++ Outcomes.java
@@ -0,0 +1,19 @@
+package Project1.FixtureGenerator;
+
+/**
+ *	Conor Finnegan 17184258
+ */
+public class Outcomes {
+    public static void main (String[] args) {
+        int homeScore, awayScore;
+        /**There a 380 fixtures, so this code will generate 380 unique outcomes*/
+        for (int i = 0; i < 379; i++) {
+            /**This generates random scores for both teams. I capped the scores at 5 as anything higher would be unlikely*/
+            homeScore = (int)(Math.random() * 5);
+            awayScore = (int)(Math.random() * 5);
+
+            /**This will print the match number, and both teams' scores*/
+            System.out.printf("%d%d", i, homeScore, awayScore);
+        }
+    }
+}
